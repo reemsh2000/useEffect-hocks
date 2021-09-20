f1db69322594862782d996c952b4d61601a83d82
reemsh2000 <sh951652@gmail.com>
2021-09-20 12:36:20 +0300

    exersise 2 sol

--- src/Components/Exersise2.jsx
+++ src/Components/Exersise2.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function Exersise2() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [x, setX] = useState();
+  const [y, setY] = useState();
+  const [color, setColor] = useState("#fff");
+  const intViewportWidth = window.innerWidth / 2;
+  useEffect(() => {
+    document.addEventListener("mousemove", (e) => {
+      setX(e.pageX);
+      setY(e.pageY);
+      x > intViewportWidth ? setColor("tomato") : setColor("blue");
+    });
+
+    return () => {
+      document.removeEventListener("mousemove",null);
+    };
+  });
+  return (
+    <div id="myDiv" style={{ background: color }}>
+      <p>
+        {" "}
+        I am on X : {x} and Y : {y}
+      </p>
+    </div>
+  );
 }
 
-export default Exersise2
+export default Exersise2;
--- src/Components/Exersise3.jsx
+++ src/Components/Exersise3.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 function Exersise3() {
+
     return (
         <div>
-            
+
         </div>
     )
 }
