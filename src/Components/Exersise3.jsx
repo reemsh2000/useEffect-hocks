763069a45c425f9fe23db0d730014627a0074f7d
reemsh2000 <sh951652@gmail.com>
2021-09-20 13:54:48 +0300

    exer 3

--- src/Components/Exersise3.jsx
+++ src/Components/Exersise3.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+// https://api.giphy.com/v1/gifs/search?api_key=&limit=2q=cry
 function Exersise3() {
+  const [value, setValue] = useState('')
+  const [result, setResult] = useState([])
+  const key = '8qSz5zbv4c0PrJvw32pQACfuEb6LLwzV'
+  useEffect(() => {
+    const getData = () => {
+      return fetch(
+        `https://api.giphy.com/v1/gifs/search?q=${value}&api_key=${key}&limit=6`,
+      ).then((res) => res.json())
+    }
+    getData().then(res=>setResult(res.data))
+    return () => {
+      document.querySelector('#search-value').removeEventListener('onChange',null)
+    }
+  }, [value])
+  return (
+    <div>
+      <input
+        id="search-value"
+        type="text"
+        placeholder="Search"
+        onChange={(e) => setValue(e.target.value)}
+        style={{margin:'15px 250px',width:'300px'}}
+      />
+      <br/>
+      <br/>
+      <div className='giff'>
+      { 
+          
+          result?result.map((ele)=>{
+           return   <img src={ele.images["480w_still"].url}alt='Gif' key={ele.id}/>
 
-    return (
-        <div>
+          // return console.log(ele.images["480w_still"].url)
 
-        </div>
-    )
+          }):''
+      }
+      </div>
+    </div>
+  )
 }
 
 export default Exersise3
