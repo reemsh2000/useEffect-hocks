97cbe28ed636dd8e0de3c875a274a4d81655f339
reemsh2000 <sh951652@gmail.com>
2021-09-20 14:41:56 +0300

    exer 4 sol

--- src/Components/Exersise4.jsx
+++ src/Components/Exersise4.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function Exersise4() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [value, setValue] = useState('https://robohash.org/reem');
+  const [result, setResult] = useState('');
+  useEffect(() => {
+    setResult(`https://robohash.org/${value}`);
+
+    return () => {
+      document
+        .querySelector("#search-value")
+        .removeEventListener("onChange", null);
+    };
+  }, [value]);
+  return (
+    <div className="conatiner">
+      <h3>GENERATE UNIQUE IMAGES FROM ANY TEXT</h3>
+      <input
+        id="search-value"
+        type="text"
+        placeholder="Search"
+        onChange={(e) => setValue(e.target.value)}
+        style={{ margin: "15px 50px", width: "300px" }}
+      />
+      <br />
+      <br />
+      <div className="giff">
+          <img src={result} alt='imag'/>
+          </div>
+    </div>
+  );
 }
 
-export default Exersise4
+export default Exersise4;
