b206a5290c5e16266e86a34d07fbf73ef226b9d1
reemsh2000 <sh951652@gmail.com>
2021-09-20 11:51:35 +0300

    exersise 1 sol

--- src/Components/Exersise1.jsx
+++ src/Components/Exersise1.jsx
@@ -2,10 +2,21 @@ import React ,{useState,useEffect} from 'react'
 
 function Exersise1() {
     const [count, setCount] = useState(0)
-    con
+    useEffect(() => {
+        // effect
+        document.addEventListener('mousedown',incrementCount)
+        return () => {
+        document.removeEventListener('mousedown')
+        }
+    }, [])
+
+    const incrementCount =()=>{
+        setCount(prevCount=>prevCount+1)
+    }
     return (
         <div className='conatiner'>
-             <span>Count :{count}</span>
+             <span style={{color:'red',fontSize:'22px'}}>Count : {count}</span>
+
         </div>
     )
 }
